630bb7f966bd6b67c235c5f0cf413ed40caf40e1
jonr13 <65420529+jonr13@users.noreply.github.com>
2020-06-09 18:10:04 -0400

    Exec Dashboard Updates

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -1,7 +1,10 @@
-import os
+import pandas as pd
 
 files = ['sales-201712', 'sales-201801', 'sales-201802', 'sales-201803', 'sales-201804', 'sales-201805', 'sales-201806', 'sales-201807', 'sales-201808', 'sales-201809', 'sales-201810', 'sales-201811', 'sales-201812', 'sales-201901', 'sales-201902', 'sales-201903', 'sales-201904']
 for fil in files:
     file_name = fil
-    path = 'data/{}.txt'.format(file_name)
-    os.remove(path)
+    path = r'data/{}.txt'.format(file_name)
+    new_path = r'data/{}.csv'.format(file_name)
+    read_file = pd.read_csv (path)
+    read_file.to_csv (new_path, index=None)
+
